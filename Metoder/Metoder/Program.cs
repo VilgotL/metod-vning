5a0c7ae59dcb40de860e6709893413c4f477a782
André <anfr01001@utb.vaxjo.se>
2019-09-20 09:37:08 +0200

    klar

---
+++ Metoder/Metoder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+        }
+        static double CirkelOmkrets(double r)
+        {
+            return (r * 2 * 3.14);
+        }
+        static double CirkelArea(double r)
+        {
+            return (r * r * 3.14);
+        }
+    }
+}
---
+++ Metoder2/Metoder2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Metoder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 72, b = 24, c = 101;
+            Console.WriteLine("Medelvärdet är "+(Medelvärde(a, b, c)));
+        }
+        static float Medelvärde(float x, float y, float z)
+        {
+            return ((x + y + z)/3);
+        }
+    }
+}
